3ba78e38f51184572d7b5b8c937016f5a61b9dd1
chenzhe <chenzhe.943@gmail.com>
2016-06-22 15:27:50 +0800

    initial

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, send_from_directory
 import requests, re, json
 from virus_total_apis import PublicApi as VirusTotalPublicApi
 
+import numpy as np
+from sklearn import manifold
+import random
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -67,6 +72,138 @@ def vt_hash(input):
 @app.route('/assets/<path:path>')
 def static_file(path):
     return send_from_directory("assets",path)
+
+@app.route("/threatcrwod/<email>")
+def threatcrowd(email):
+    req = requests.get("https://www.threatcrowd.org/searchApi/v2/email/report/", params={"email": email})
+    j = json.loads(req.text)
+    K = 3
+
+    # compute distance between two domains
+    def domain_similarity(s1, s2):
+
+        if len(s1) > len(s2):
+            s1, s2 = s2, s1
+        distances = range(len(s1) + 1)
+
+        for i2, c2 in enumerate(s2):
+            distances_ = [i2 + 1]
+            for i1, c1 in enumerate(s1):
+                if c1 == c2:
+                    distances_.append(distances[i1])
+                else:
+                    distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+            distances = distances_
+        return distances[-1]
+
+    # compute distance matrix for all domains
+    count = len(j['domains'])
+    similarity = []
+    for count_index1 in range(0, count):
+        tmp = []
+        for count_index2 in range(0, count):
+            if count_index1 == count_index2:
+                simi = 0
+            elif count_index1 < count_index2:
+                simi = domain_similarity(j['domains'][count_index1], j['domains'][count_index2])
+            else:
+                simi = similarity[count_index2][count_index1]
+            tmp.append(simi)
+        similarity.append(tmp)
+
+    # scale the distance matrix
+    adist = np.array(similarity)
+    adist = adist * 10
+
+    # compute coordinates matrix
+    mds = manifold.MDS(n_components=2, dissimilarity="precomputed", random_state=6)
+    results = mds.fit(adist)
+    coords = results.embedding_
+
+    # clustering all points according to given centroid
+    def cluster_points(X, mu):
+        clusters = {}
+        for x in X:
+            bestmukey = min([(i[0], np.linalg.norm(x - mu[i[0]])) \
+                             for i in enumerate(mu)], key=lambda t: t[1])[0]
+            try:
+                clusters[bestmukey].append(x)
+            except KeyError:
+                clusters[bestmukey] = [x]
+        return clusters
+
+    # relocate centroids
+    def reevaluate_centers(mu, clusters):
+        newmu = []
+        keys = sorted(clusters.keys())
+        for key in keys:
+            newmu.append(np.mean(clusters[key], axis=0))
+        return newmu
+
+    # check convergence of centroids
+    def has_converged(mu, oldmu):
+        return (set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu]))
+
+    # find stable centroids
+    def find_centroids(X, k):
+        # Initialize to K random centers
+        oldmu = random.sample(X, k)
+        mu = random.sample(X, k)
+        while not has_converged(mu, oldmu):
+            oldmu = mu
+            # Assign all points in X to clusters
+            clusters = cluster_points(X, mu)
+            # Reevaluate centers
+            mu = reevaluate_centers(oldmu, clusters)
+        return (mu, clusters)
+
+    # Euclidean distance
+    def Eu_distance(P1, P2):
+        dist = np.sqrt(pow((P1[0] - P2[0]), 2) + pow((P1[1] - P2[1]), 2))
+        return dist
+
+    class domain_coordinates:
+        'for storing domain instances'
+
+        def __init__(self, x_coordinate, y_coordinate, cluster, domain_name, is_center):
+            self.x_coordinate = x_coordinate
+            self.y_coordinate = y_coordinate
+            self.cluster = cluster
+            self.domain_name = domain_name
+            self.is_center = is_center
+
+    # Find corresponding domain name for given coordinates
+    def find_domain(coordinates):
+        for m in range(0, count):
+            if coordinates[0] == int(coords[m][0]) and coordinates[1] == int(coords[m][1]):
+                return str(j['domains'][m])
+
+    def find_result(X, k):
+        (M, C) = find_centroids(X, k)
+        result = {}
+        # change to integer coordinates
+        for l in range(0, k):
+            for point_index in range(0, len(C[l])):
+                C[l][point_index] = [int(C[l][point_index][0]), int(C[l][point_index][1])]
+        # find acutal center
+        for i in range(0, k):
+            dis_array = []
+            for point in C[i]:
+                dis_array.append(Eu_distance(point, M[i]))
+            index = dis_array.index(min(dis_array))
+            # Store center
+            center_point = C[i].pop(index)
+            result[str(i)] = [[center_point[0], center_point[1], find_domain(center_point)]]
+
+            for k in range(0, len(C[i])):
+                if k != index and k <= 3:
+                    # Store first 3 non-center domains
+                    result[str(i)].append([C[i][k][0], C[i][k][1], find_domain(C[i][k])])
+
+        return result
+
+    return render_template("visualization.htm", data=json.dumps(find_result(coords, K)))
+
 
 if __name__ == "__main__":
     app.run()
