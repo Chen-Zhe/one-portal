8393c84c9424797c7fb6257a203e4762a49b7700
chenzhe <chenzhe.943@gmail.com>
2016-06-22 11:47:51 +0800

    initial

--- hello.py
+++ hello.py
@@ -1,9 +1,33 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+import requests, re, json
 
 @app.route("/")
 def hello():
-    return render_template("test.html",data="hello world")
+    return render_template("test.html")
+
+
+@app.route("/whois/<text>")
+def whois(text):
+    check_ip = text.split(".")[-1]
+    try:
+        int(check_ip)
+        result_text = requests.get("https://who.is/whois-ip/ip-address/" + text,
+                                   headers={"User-Agent": "Mozilla/5.0"}).text
+        whois_text = re.match('.*<div class="col-md-12 queryResponseBodyKey"><pre>(.*)</pre></div>', result_text,
+                              re.DOTALL).group(1)
+        return render_template("whois.html", text=whois_text)
+    except:
+        result_text = requests.get(
+            "http://api.whoapi.com/?apikey=3fb4ea768efd677bfbed2d705bc6f47a&r=whois&domain=" + text,
+            headers={"User-Agent": "Mozilla/5.0"}).text
+        if result_text[0]=="{":
+            whois_dict = json.loads(result_text)
+            reply = "registered: " + whois_dict["registered"] + "date created: " + whois_dict["date_created"]
+        else:
+            reply = "no such domain in record"
+        return render_template("whois.html", text=reply.replace("\n", "<br>"))
+
 
 if __name__ == "__main__":
     app.run()
